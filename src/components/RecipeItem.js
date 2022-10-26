cca5de71d7671e1ec7dbbd0ed3d5ae94884473d8
miaor5 <ortiz.maria.mero@gmail.com>
2022-10-27 01:38:46 +0200

    list style

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 function RecipeItem(props) {
   return (
-    <Link to={`/recipe/${props.recipe.id}`} className="no">
-      <img src={props.recipe.image} alt="" />
-      <h4>{props.recipe.recipeName}</h4>
+    <Link to={`/recipe/${props.recipe.id}`} className="recipe">
+      <img src={props.recipe.image} alt="" className="recipe_image" />
+      <h4 className="recipe_title">{props.recipe.recipeName}</h4>
     </Link>
   );
 }
