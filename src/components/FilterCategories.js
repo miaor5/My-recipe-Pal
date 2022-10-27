6059c368496d0ec4c901309debfb127d1a586df2
miaor5 <ortiz.maria.mero@gmail.com>
2022-10-27 02:03:47 +0200

    categotyfilter

--- src/components/FilterCategories.js
+++ src/components/FilterCategories.js
@@ -1,4 +1,18 @@
-function FilterCategorie() {
+function FilterCategorie(props, ev) {
+  const renderCategory = (ev) => {
+    return props.getYear.map((year, index) => {
+      return (
+        <option key={index} value={year}>
+          {year}
+        </option>
+      );
+    });
+  };
+
+  const handleChange = (ev) => {
+    props.handleFilterYear(ev.target.value);
+  };
+
   return (
     <div className="main__filterCategorie">
       <label className="main__filterCategorie--label">Type of food:</label>
